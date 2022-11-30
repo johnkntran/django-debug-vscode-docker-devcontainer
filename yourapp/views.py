c8c01a8dde16c498c348380a98d98bee1b4e87e7
Your Name <yourname@you.com>
2022-11-30 04:06:19 +0000

    make view async

--- yourapp/views.py
+++ yourapp/views.py
@@ -1,6 +1,6 @@
 from django.http import JsonResponse
 
 
-def some_endpoint(request):
+async def some_endpoint(request):
     msg = 'Hello, World!' # TODO: Set breakpoint here to test debugging
-    return JsonResponse({'msg': msg})
+    return JsonResponse({'message': msg})
